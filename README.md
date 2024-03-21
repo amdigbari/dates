e759b462915a722683e7e14be16521b19baceedc
amdigbari <33571407+amdigbari@users.noreply.github.com>
2024-03-21 05:58:18 +0330

    Initialized the Media Manager Project (#1)
    Co-authored-by: amirmahdidigbari@yahoo.com <amdigbari@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# dates
+# Dates
---
+++ media_manager/src/main/kotlin/media_manager/plugins/Serialization.kt
@@ -0,0 +1,18 @@
+package media_manager.plugins
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+    routing {
+        get("/json/kotlinx-serialization") {
+                call.respond(mapOf("hello" to "world"))
+            }
+    }
+}
