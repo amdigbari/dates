ba16828320a44ed4f7f283ccaa8b426a856520b0
amirmahdidigbari@yahoo.com <amdigbari@gmail.com>
2024-05-04 16:15:26 +0330

    Add responsiveness to login and register pages

--- media_hub/src/app/auth/layout.tsx
+++ media_hub/src/app/auth/layout.tsx
@@ -6,10 +6,25 @@ export default function AuthLayout({ children }: LayoutProps) {
   return (
     <div className={styles['container']}>
       <div className={styles['graphics-wrapper']}>
-        <Image src="/images/layout-background.jpeg" alt="couple at beach" fill />
+        {/* Desktop */}
+        <Image
+          src="/images/auth/layout-background-desktop.jpeg"
+          alt="couple at a beach"
+          fill
+          className="d-none d-md-block"
+        />
+
+        {/* Mobile */}
+        <Image
+          src="/images/auth/layout-background-mobile.jpeg"
+          alt="couple at a beach"
+          fill
+          className="d-block d-md-none"
+        />
+
+        <div className={styles['graphics-wrapper_overlay']} />
       </div>
 
-      <div className={styles['overlay']} />
       <div className={styles['children-wrapper']}>
         <div className={styles['children-wrapper_children']}>{children}</div>
       </div>
