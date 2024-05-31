8610730f10dea40b468f7f1ac9e1e6a242772578
amdigbari <33571407+amdigbari@users.noreply.github.com>
2024-05-31 04:07:10 +0330

    Add Sign method for s3 objects (#20)
    Co-authored-by: amdigbari <amdigbari@gmail.com>

---
+++ media_manager/src/main/kotlin/mediaManager/s3/S3ObjectMode.kt
@@ -0,0 +1,6 @@
+package mediaManager.s3
+
+enum class S3ObjectMode {
+    PUBLIC,
+    PRIVATE,
+}
--- media_manager/src/main/kotlin/mediaManager/s3/S3Properties.kt
+++ media_manager/src/main/kotlin/mediaManager/s3/S3Properties.kt
@@ -3,4 +3,11 @@ package mediaManager.s3
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("s3")
-data class S3Properties(val endpoint: String, val accessKey: String, val secretKey: String, val bucketName: String)
+data class S3Properties(
+    val endpoint: String,
+    val accessKey: String,
+    val secretKey: String,
+    val bucketName: String,
+    val publicDir: String,
+    val privateDir: String,
+)
